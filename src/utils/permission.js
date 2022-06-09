b27813d706030bf9448a7155581f31714f24c63f
袁鹏 <rocyuan666@163.com>
2022-06-09 09:24:21 +0800

    permission

--- src/utils/permission.js
+++ src/utils/permission.js
@@ -6,47 +6,46 @@ import 'nprogress/nprogress.css' // progress bar style
 import { getToken } from '@/utils/auth' // get token from cookie
 import getPageTitle from '@/utils/get-page-title'
 
-NProgress.configure({ showSpinner: false }) // NProgress Configuration
+NProgress.configure({ showSpinner: false }) // NProgress 配置
 
-const whiteList = ['/login'] // no redirect whitelist
+const whiteList = ['/login'] // 无重定向白名单
 
 router.beforeEach(async(to, from, next) => {
-  // start progress bar
   NProgress.start()
 
-  // set page title
   document.title = getPageTitle(to.meta.title)
 
-  // determine whether the user has logged in
+  // 确定用户是否已登录
   const hasToken = getToken()
 
   if (hasToken) {
     if (to.path === '/login') {
-      // if is logged in, redirect to the home page
+      // 如果已登录，请重定向到主页
       next({ path: '/' })
       NProgress.done()
     } else {
-      // determine whether the user has obtained his permission roles through getInfo
+      // 确定用户是否已通过 getInfo 获得其权限角色
       const hasRoles = store.getters['user/roles'] && store.getters['user/roles'].length > 0
       if (hasRoles) {
         next()
       } else {
         try {
-          // get user info
-          // note: roles must be a object array! such as: ['admin'] or ,['developer','editor']
+          // 获取用户信息
+          // 注意：角色必须是对象数组！例如：['admin'] 或 ['developer'、'editor']
           const { roles } = await store.dispatch('user/getInfo')
 
-          // generate accessible routes map based on roles
+          // 基于角色生成可访问的路由
           const accessRoutes = await store.dispatch('permission/generateRoutes', roles)
 
-          // dynamically add accessible routes
+          // 动态添加可访问的路由
           router.addRoutes(accessRoutes)
 
-          // hack method to ensure that addRoutes is complete
-          // set the replace: true, so the navigation will not leave a history record
+          // 确保 addRoutes 完整的hack方法
+          // 将replace设置为true，以便导航不会留下历史记录
           next({ ...to, replace: true })
         } catch (error) {
           // remove token and go to login page to re-login
+          // 删除 token 并转到登录页面以重新登录
           await store.dispatch('user/resetToken')
           Message.error(error || 'Has Error')
           next(`/login?redirect=${to.path}`)
@@ -55,13 +54,13 @@ router.beforeEach(async(to, from, next) => {
       }
     }
   } else {
-    /* has no token*/
+    // 没有token
 
     if (whiteList.indexOf(to.path) !== -1) {
-      // in the free login whitelist, go directly
+      // 在免费登录白名单中，直接进入
       next()
     } else {
-      // other pages that do not have permission to access are redirected to the login page.
+      // 其他没有访问权限的页面将重定向到登录页面。
       next(`/login?redirect=${to.path}`)
       NProgress.done()
     }
