4c205882d1185fe2929bb07ef0b4cd953ff25e60
袁鹏 <rocyuan666@163.com>
2022-06-09 09:56:08 +0800

    directive

--- src/store/modules/permission.js
+++ src/store/modules/permission.js
@@ -1,7 +1,7 @@
 import { asyncRoutes, constantRoutes } from '@/router'
 
 /**
- * Use meta.role to determine if the current user has permission
+ * 使用 meta.role 用于确定当前用户是否具有权限的角色
  * @param roles
  * @param route
  */
@@ -14,7 +14,7 @@ function hasPermission(roles, route) {
 }
 
 /**
- * Filter asynchronous routing tables by recursion
+ * 通过递归过滤 异步路由表
  * @param routes asyncRoutes
  * @param roles
  */
